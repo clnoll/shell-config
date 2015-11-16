b3fcbb7fab60786c306047c088f076471c2e3e7e
Catherine Noll <catherine.noll@ampush.com>
2015-11-15 16:36:58 -0800

    Delete unused bashrc and clean up bashrc

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -8,12 +8,11 @@ source lib.sh
 source path.sh
 source git-functions.sh
 source env.sh
-# # __dan_is_osx && source osx_env.sh
+# __dan_is_osx && source osx_env.sh
 source dircolors.sh
 source prompt.sh
 source bash/history.sh
 source bash/completion.sh
-# source ~/configfiles/git-completion.bash
 # source fasd.sh
 source readline.sh
 source alias.sh
